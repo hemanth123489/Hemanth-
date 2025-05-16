56d3b8ed020d0be81452940a765bf7b4a901b58d
hemanth123489 <gowdabharath945@gmail.com>
2025-05-16 14:55:49 +0530

    Create lower case .c

---
+++ lower case .c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+ char ch;
+ printf("Hi Hemanth!\n");
+ printf("enter any character (both upper and lower case):");
+ scanf("%c",&ch);
+ printf("THE ASCII value of %c is : %d ",ch,ch);
+ return 0;
+}
