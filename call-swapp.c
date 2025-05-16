a611b546dcc8e7f196457d940009be9ce6689de7
hemanth123489 <gowdabharath945@gmail.com>
2025-05-16 12:44:45 +0530

    Create call-swapp.c

---
+++ call-swapp.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int   x=10,y=20,z;
+    int *ptr1=&x;
+    int *ptr2=&y;
+    printf("before swapx =%d y=%d",x,y);
+    z=*ptr1;
+    *ptr1=*ptr2;
+    *ptr2=z;
+    printf("after swapy =%d y=%d\n,x,y");
+}
