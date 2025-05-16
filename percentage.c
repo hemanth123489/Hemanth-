aaca548386eeb125088f24b3d62da2ffd51b1cd8
hemanth123489 <gowdabharath945@gmail.com>
2025-05-16 14:42:15 +0530

    Create percentage.c

---
+++ percentage.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int mark;
+    float percent;
+    printf("Hi Hemanth!\n");
+    printf("enter you total marks and percentage:");
+    scanf("%d%f",&mark,&percent);
+    printf("total marks is :%d\n",mark);
+    printf("percentage is :%.3f",percent);
+    return 0;
+}
