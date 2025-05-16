2606a7beed5670b231272fa820d1d5aadb46a051
hemanth123489 <gowdabharath945@gmail.com>
2025-05-16 13:05:34 +0530

    Create character.c

---
+++ character.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+int main(){
+    char ch;
+    printf("enter a character:");
+    scanf("%c",&ch);
+    printf("the char value of '%' is %d\n",ch ,ch);
+    
+    return 0;
+}
