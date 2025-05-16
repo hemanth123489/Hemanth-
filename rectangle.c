bb7274f9250295fe530cf5e402307e61c6064f2c
hemanth123489 <gowdabharath945@gmail.com>
2025-05-16 14:51:05 +0530

    Create rectangle.c

---
+++ rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int  len,wid,area;
+    printf("Hi Hemanth!\n");
+    printf("enter the length and width of rectangle:");
+    scanf("%d%d",&len,&wid);
+    area=len*wid;
+    printf("area of rectangle is:%d",area);
+    return 0;
+}
