ca0f8b831e65e25aeb4930c45bab93c08b723a0b
hemanth123489 <gowdabharath945@gmail.com>
2025-05-16 14:28:40 +0530

    Create sum of a and b.c

---
+++ sum of a and b.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("Hi Hemanth!\n");
+    printf("enter the value of a:");
+    scanf("%d",&a);
+    printf("enter the  value of b:");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("sum of %d and %d is:%d",a,b,sum);
+    return 0;
+}
