64b1a4f76bc17601e85cf5948aabf87fe4897fde
hemanth123489 <gowdabharath945@gmail.com>
2025-05-16 13:03:36 +0530

    Create two integer.c

---
+++ two integer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+     int a,b,temp;
+     printf("enter frist integer (a):");
+     scanf("%d",&a);
+     printf("enter second integer (b):");
+     scanf("%d",&b);
+     temp=a;
+     a=b;
+     b=temp;
+     printf("after swapp6:\n");
+     printf("a=%d\n,a");
+     printf("b=%d\n,b");
+     
+     return 0;
+}
